75d897478b813243de0eb2fcbb72df058f245e3e
wasi hasan <wasihasan41@gmail.com>
2022-03-17 13:14:44 +0530

    js started

---
+++ src/javascript.js
@@ -0,0 +1,55 @@
+// # API
+console.log("This is text");
+console.warn("This is warning");
+console.error("this is error");
+
+// # js variables
+// variable = container to store data values.
+var number1 = 28;
+var number2 = 22;
+console.log(number1 + number2);
+
+// # js data 
+// numebrs
+var num1 = 25;
+var num2 = 48.60;
+// strings
+var str1 = "this is a strign";
+var str2 = "this is also a string";
+// objects
+var marks = {
+    ravi: 34,
+    ajay: 45,
+    wasi: 49
+}
+console.log(marks);
+
+// # booleans
+// var true false
+var a = true;
+var b = false;
+console.log(a, b);
+// var und = undefined
+var und = undefined;
+console.log(und);
+// var null
+var n = null;
+console.log(n);
+
+// # arrays
+var arr = [1, 2, 3, 4, 5]
+console.log(arr)
+// if you want to show a specific numeber like 2 then:
+console.log(arr[2])
+
+// also can add strings to arrays:
+var arr = [1, "wasi", 3, 4 ]
+console.log(arr[1])
+
+/* At a very high level, there are two types of data types in javascript
+1. primitive data type: undefined, null, number, string, booleans, symbol 
+2. reference data type: arrays and objects
+*/
+
+
+
