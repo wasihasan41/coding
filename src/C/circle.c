f6748fe1e9ad8a4b73d0c8d70ecf3de54546873e
wasi hasan <wasihasan41@gmail.com>
2022-04-14 20:39:14 +0530

    c exercise

--- src/C/circle.c
+++ src/C/circle.c
@@ -8,7 +8,9 @@ int main(){
     printf("write radius of the circle\n");
     scanf("%d", &r);
 
-    printf("area of rectangle is %.2f", pie * (r * r));
+    printf("area of circle is %.2f\n", pie * (r * r));
+    printf("diameter of circle is %d\n", 2 * r);
+    printf("perimeter of circle is %.2f", 2 * pie * r);
 
     return 0;
 }
--- src/C/rectangle.c
+++ src/C/rectangle.c
@@ -6,7 +6,8 @@ int main(){
     printf("write length and breadth\n");
     scanf("%d %d", &length, &breadth);
 
-    printf("area of rectangle is %d", length * breadth);
+    printf("area of rectangle is %d\n", length * breadth);
+    printf("perimeter of rectangle is %d", 2 * (length + breadth));
 
     return 0;
 }
