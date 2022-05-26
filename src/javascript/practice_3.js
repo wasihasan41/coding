84463f0c28e16c0d3c5535af020bf856c24da1d3
wasi hasan <wasihasan41@gmail.com>
2022-05-26 21:21:27 +0530

    chrome ext

---
+++ src/javascript/practice_3.js
@@ -0,0 +1,12 @@
+function button1(){
+    document.getElementById("item1").classList.add("active")
+}
+function button2(){
+    document.getElementById("item2").classList.add("active")
+}
+function button3(){
+    document.getElementById("item3").classList.add("active")
+}
+function button4(){
+    document.getElementById("item4").classList.add("active")
+}
